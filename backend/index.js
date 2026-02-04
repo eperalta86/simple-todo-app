654676d097cde76a8fb202fe447723b77d19e358
Esteban Peralta <esteban.peralta.galarce@gmail.com>
2026-02-04 12:11:46 -0300

    get post test

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,35 @@
-const express = require('express'); // Es como el 'import' de Java
+const express = require('express'); // import alike
 const app = express();
 const PORT = 3000;
 
-// Esto es como un @GetMapping("/")
-app.get('/', (req, res) => {
-  res.send('¡Paralapapiricoipi!');
+app.use(express.json());
+
+//fake data
+let tasks = [
+    { id: 1, title: "Task 1", completed: false },
+    { id: 2, title: "Task 2", completed: true }
+];
+
+// getmapping alike
+ app.get('/', (req, res) => {
+   res.send('Test');
+});
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks); //array to json.
+});
+
+//fake add
+app.post('/tasks', (req, res) => {
+    const newTask = {
+        id: tasks.length + 1, //fake autoid
+        title: req.body.title,
+        completed: false
+    };
+
+    tasks.push(newTask);
+    
+    res.status(201).json(newTask);
 });
 
 app.listen(PORT, () => {
